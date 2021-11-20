8475e2795941065ebb8d49d32dd0af1840a8547a
strongest <acce2011@yandex.ru>
2021-11-20 21:31:45 +0300

    Задание 4 по извлечению корня. Условие: Необходимо вычислить квадратный корень числа, не используя готовую реализации метода Math.sqrt, а полагаясь на простые арифметические операции

--- src/main/java/Lesson4Program.java
+++ src/main/java/Lesson4Program.java
@@ -1,30 +1,17 @@
 public class Lesson4Program {
     public static void main(String[] args) {
-        System.out.println("Квадра́тный ко́рень из числа a (корень 2-й степени) — число x, \n" +
-                "дающее a при возведении в квадрат: x ⋅ x = a. \n" +
-                "Равносильное определение: квадратный корень из числа a — решение уравнения \n" +
-                "x2(в квадрате) = a. (x^{2}=a) \n" +
-                "Операция вычисления значения квадратного корня из числа a называется \n " + "" +
-                "«извлечением квадратного корня» из этого числа.");
+        System.out.println("Условие задания курса: Необходимо вычислить квадратный корень числа, не используя \n" +
+                "готовую реализации метода Math.sqrt, а полагаясь на простые арифметические операции");
 
-        // возведение числа
-        double vozvedenie = vozvedenie (5);
-        System.out.println("Возведение числа в квадратный корень =" + vozvedenie);
+        // извлечение корня с помощью цикла
 
-        // извлечение корня
-        double izvlechenie = izvlechenie (5, vozvedenie);
-        System.out.println("Извлечение корня =" + izvlechenie);
-        //Scanner Scanner = new Scanner(System.in);
-        //String vozvedenie = scanner.nextDouble();
-        //System.out.println("Извлечение корня =" + izvlechenie);
+        double n = 4;
+        for (double i = 1; i < 1000; i++) {
+            if (i * i == n)
+                System.out.println("Корень числа n = " + (i));
+        }
+
+
     }
 
-    public static double vozvedenie (double arg1) {
-        double result = arg1 * arg1;
-        return result;
-    }
-    public static double izvlechenie (double arg1, double vozvedenie) {
-        double result1 = vozvedenie / arg1;
-        return result1;
-    }
 }
